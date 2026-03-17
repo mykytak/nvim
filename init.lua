7657f571c8025191f3c7054d3707f1a20deaf2c9
Mykytak <mykytak.ua@gmail.com>
2026-03-17 19:01:31 +0200

    set delay for which-key popup

--- init.lua
+++ init.lua
@@ -221,9 +221,7 @@ require("lazy").setup({
       "folke/which-key.nvim",
       event = "VeryLazy",
       opts = {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
+        delay = 1000,
       },
       keys = {
         {
